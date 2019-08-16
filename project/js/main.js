6532269e112abc2f10e0968ddc5d37cb28c383c6
Emre Demirel <emred2700@gmail.com>
2019-08-16 21:47:03 +0200

    This is the last commit and about databases

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,63 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+$(document).ready(function(){
+  getPosts();
+})
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
+function addMessage(postTitle, postBody){
+  const postData = {
+    title:postTitle,
+    body:postBody
+  }
+
+  // Get a reference to the database service
+  var database = firebase.database().ref('posts');
+
+  // Create a new post reference with an auto-generated id
+var newPostRef = database.push();
+newPostRef.set(postData, function(error) {
+  if (error) {
+    // The write failed...
+  } else {
+    window.location.reload();
+  }
+});
+}
+
+function handleMessageFormSubmit(){
+  const postTitle = $('#post-title').val();
+  const postBody = $('#post-body').val();
+  addMessage(postTitle, postBody);
+}
+
+function getPosts(){
+
+  return firebase.database().ref('posts').once('value').then(function(snapshot) {
+    var posts = snapshot.val();
+    for(var postKey in posts){
+      var post = posts[postKey];
+      $('#post-listing').append(`<div>${post.title} - ${post.body}</div>`);
+    }
+  });
+
 }
